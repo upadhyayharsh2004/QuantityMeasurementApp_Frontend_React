d6dd47d66d910760af196d5d9620c61b2005372f
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-04-17 14:26:48 +0530

    fix routing for vercel

--- UC20/src/services/api.js
+++ UC20/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'https://quantitymeasurementappbackend-production.up.railway.app';
 
 // Create axios instance
 const api = axios.create({
